c09dc9943d0cc6a0539df2a07f6d53c1beacae7b
Kofise17 <62119634+Kofise17@users.noreply.github.com>
2025-05-30 14:19:15 +0200

    Created TodoItem model

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public int Id {  get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
